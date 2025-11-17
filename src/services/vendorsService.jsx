874a7a7c8b0d9b49990e26d8f30f5f0f5a0967fe
Mohammed Irfan <mohammed_irfan@distrbutox.com>
2025-11-17 17:42:56 +0530

    fix: changes the project

--- src/services/vendorsService.jsx
+++
@@ -1,6 +0,0 @@
-import api from "../api/Api.jsx";
-
-export const getVendors = () => api.get("/vendors");
-export const createVendor = (vendor) => api.post("/vendors", vendor);
-export const updateVendor = (id, vendor) => api.put(`/vendors/${id}`, vendor);
-export const deleteVendor = (id) => api.delete(`/vendors/${id}`);
